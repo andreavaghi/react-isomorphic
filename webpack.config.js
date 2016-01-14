bd70fd7b2a590b9d28f3b52043ec3d9ebb830eaa
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2016-01-14 17:23:01 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const CLIENT_DIR = path.resolve(__dirname, 'client');
+const SERVER_DIR = path.resolve(__dirname, 'server/generated');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+
+const loaders = [
+  {
+    test: /\.js$/,
+    include: CLIENT_DIR,
+    loader: 'babel-loader',
+    query: {
+      presets: ['es2015', 'react']
+    }
+  },
+  {
+    test: /\.scss$/,
+    loader: 'style!css!sass'
+  }
+];
+
+module.exports = [{
+  name: 'client',
+  target: 'web',
+  context: CLIENT_DIR,
+  entry: './index.js',
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: loaders
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(CLIENT_DIR, 'components')
+    }
+  }
+},
+{
+  name: 'server',
+  target: 'node',
+  context: CLIENT_DIR,
+  entry: {
+    app: 'components/app/index.js'
+  },
+  output: {
+    path: SERVER_DIR,
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: /^[a-z\-0-9]+$/,
+  module: {
+    loaders: loaders
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(CLIENT_DIR, 'components')
+    }
+  }
+}];
