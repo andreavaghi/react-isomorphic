30ca66c899a83ce3be79411c5fe2fbc295c21ed2
Andrea Vaghi <andrea.vaghi@gmail.com>
2016-02-16 08:34:41 +0100

    add first component

---
+++ client/components/app/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {} from './style.scss';
+
+class App extends Component {
+  render() {
+    return <div>Hello from a React Component</div>;
+  }
+}
+
+export default App;
