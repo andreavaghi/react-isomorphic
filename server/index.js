25ed0f65a2d8cd91048fedd1b740ccc7806af71b
Andrea Vaghi <andrea.vaghi@gmail.com>
2016-02-16 19:49:27 +0100

    add handlebars templating

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
+import path from 'path';
 import express from 'express';
+import handlebars from 'express-handlebars';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from './generated/app';
 
 const app = express();
 
+app.engine('.hbs', handlebars({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
 app.get('/', (req, res) => {
-  res.send(ReactDOMServer.renderToString(<App />));
+  res.render('app', {
+    app: ReactDOMServer.renderToString(<App />)
+  });
 });
 
 app.listen(3000, () => console.log('server running'));
